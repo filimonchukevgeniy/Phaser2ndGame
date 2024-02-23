de14b659dbfd82687fbfd5b00fbcda09c44ec559
filimonchukevgeniy <23a_fyev@liceum.ztu.edu.ua>
2024-02-23 13:00:40 +0200

    0.0.0
    Додав фон , головного героя , почав додавати платформи , всі текстурки будуть потім перероблені

--- game.js
+++ game.js
@@ -1,15 +1,41 @@
-const config = {
+var config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
-    scene: Main, // Цю сцену ми створимо на 4-му кроці
-    backgroundColor: '#71c5cf',
+    // для this.physics
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 0}
+            gravity: { y: 300 },
+            debug: false
         }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
 };
 
 const game = new Phaser.Game(config);
+
+
+function preload ()
+{
+    this.load.image("hero", "assets/hero.png");
+    this.load.image("sky" , "assets/Sky.png");
+    this.load.image("block", "assets/block.png");
+}
+
+function create()
+{
+    
+    this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    this.plane = this.physics.add.sprite(100, 100, 'hero');
+    platforms = this.physics.add.staticGroup();
+}
+
+function update()
+{
+
+}
